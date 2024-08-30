6d848f7db5304b0bcb493ac6cef26336f0c77065
Edson <101560941+edi-gr@users.noreply.github.com>
2024-08-30 22:52:40 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <div className="w-10/12 mx-auto my-10">
         <div className="flex items-start text-2xl text-neutral-300">
           <p className="text-5xl mr-2 mt-[2px]">&#169;</p> Edson{" "}
-          <small className="mt-[2px] ml-2">2023</small>{" "}
+          <small className="mt-[2px] ml-2">2024</small>{" "}
         </div>
       </div>
     </div>
